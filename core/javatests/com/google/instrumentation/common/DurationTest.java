c941d0fe9d0b786bda8f5d17ccbcdbad69ad59f4
Kristen Kozak <sebright@google.com>
2016-12-07 10:32:28 -0800

    Upgrade Bazel.

--- core/javatests/com/google/instrumentation/common/DurationTest.java
+++ core/javatests/com/google/instrumentation/common/DurationTest.java
@@ -8,6 +8,12 @@ import org.junit.Test;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.logging.Level;
 
 /** Unit tests for {@link Duration}. */
 @RunWith(JUnit4.class)
